a4bb1394d55dfecbc4805fa36539a0a62feae592
Brian Ketelsen <bketelsen@gmail.com>
2026-03-13 14:14:55 -0400

    feat: add CSS styling for chat bubbles and activity panel

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -4,6 +4,7 @@ import GLib from "gi://GLib";
 import Gio from "gi://Gio";
 import Gtk from "gi://Gtk?version=4.0";
 import Adw from "gi://Adw?version=1";
+import Gdk from "gi://Gdk?version=4.0";
 
 // Promisify async methods we'll use
 Gio._promisify(Gio.DataInputStream.prototype, "read_line_async", "read_line_finish_utf8");
@@ -15,6 +16,21 @@ import { FrostyWindow } from "./window.js";
 const application = new Adw.Application({
   application_id: "com.frostyard.Frosty",
   flags: Gio.ApplicationFlags.DEFAULT_FLAGS,
+});
+
+application.connect("startup", (_app) => {
+  const cssProvider = new Gtk.CssProvider();
+  // Use import.meta.url for reliable path resolution in ESM Gjs
+  const thisDir = GLib.path_get_dirname(
+    GLib.filename_from_uri(import.meta.url)[0],
+  );
+  const cssPath = GLib.build_filenamev([thisDir, "style.css"]);
+  cssProvider.load_from_path(cssPath);
+  Gtk.StyleContext.add_provider_for_display(
+    Gdk.Display.get_default(),
+    cssProvider,
+    Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION,
+  );
 });
 
 let frostyWindow = null;
